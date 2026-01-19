76b60d1915b20ad1f9d96618ae25391312ccd0f3
Erick Freitas <erick.freitas@trinca.com>
2026-01-19 13:52:40 -0300

    refactor: simplify AdminPage component by removing title and defaultRedirect props
    - Updated AdminPage to render LoginView without title and defaultRedirect props for a cleaner implementation.

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -3,5 +3,5 @@
 import { LoginView } from '@/@components/views/admin/auth/login';
 
 export default function AdminPage() {
-  return <LoginView title="Login Admin" defaultRedirect="/admin/appointments" />;
+  return <LoginView />;
 }
